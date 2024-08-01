3958d36d687cc202e5d49ccf328c675590cea0c0
samuel marcos <samuel.mf1998@gmail.com>
2024-08-01 22:55:23 +0100

    se añaden los recursos de clientes

--- main/resources/__init__.py
+++ main/resources/__init__.py
@@ -1,5 +1,5 @@
-#from .clientes import Clientes as ClientesResource
-#from .clientes import Cliente as  ClienteResource
+from .clientes import Clientes as ClientesResource
+from .clientes import Cliente as  ClienteResource
 from .usuarios import Usuarios as UsuariosResource
 from .usuarios import Usuario as UsuarioResource
 from .compras import Compras as ComprasResource
